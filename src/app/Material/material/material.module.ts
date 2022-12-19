58c3cb3afba3121e3027a461dadb30046d188774
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-19 16:10:19 +0530

    seperate module for material

---
+++ src/app/Material/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule { }
