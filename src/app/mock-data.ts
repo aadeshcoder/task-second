27cda597c1a18f232911c8b84e88b9eb0a5d9790
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-21 16:46:58 +0530

    mock data for sell cards

---
+++ src/app/mock-data.ts
@@ -0,0 +1,27 @@
+import { SellData } from "./SellData"
+export const sellsData: SellData[] = [
+    {
+        id: 1,
+        name: "New Users",
+        sell: "12k",
+        change: 50,
+    },
+    {
+        id: 2,
+        name: "Users retension",
+        sell: "10k",
+        change: 40,
+    },
+    {
+        id: 3,
+        name: "User engagment",
+        sell: "2k",
+        change: -20,
+    },
+    {
+        id: 4,
+        name: "Referral",
+        sell: "1k",
+        change: -50,
+    }
+] 
