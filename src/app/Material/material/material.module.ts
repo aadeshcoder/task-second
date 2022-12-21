ee600cfe3893b18b2b1fd9b64ca41411e54fd0e4
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-21 16:49:32 +0530

    added more module

--- src/app/Material/material/material.module.ts
+++ src/app/Material/material/material.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
   declarations: [],
   exports: [
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class MaterialModule { }
