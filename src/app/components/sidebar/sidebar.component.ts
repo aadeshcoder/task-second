97c2fb13ad48be1466491ef29b4c45a32692f119
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-21 16:49:18 +0530

    updated sidebar component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { SidebarExpandService } from 'src/app/services/sidebar-expand.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  constructor (public sidebarExpand: SidebarExpandService) {}
+  
 }
