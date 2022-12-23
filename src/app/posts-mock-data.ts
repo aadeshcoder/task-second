23e5afc1166e421f834c6cd60782c2b9c2956bf2
aadeshcoder <aadeshtendolkar@gmail.com>
2022-12-23 09:41:43 +0530

    mock data for posts

---
+++ src/app/posts-mock-data.ts
@@ -0,0 +1,51 @@
+import { Post } from "./Post"
+export const posts: Post[] = [
+    {
+        id: 1,
+        name: "Arjun",
+        title: "My Journey",
+        postedOn: "12/12/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi. Lorem ipsum dolor sit amet cons",
+        restrictedTo: [],
+    },
+    {
+        id: 2,
+        name: "Kushagra",
+        title: "How I started",
+        postedOn: "10/11/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi. Lorem ipsum dolor sit amet cons Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut tempora incidunt!",
+        restrictedTo: [],
+    },
+    {
+        id: 3,
+        name: "Netra",
+        title: "Do this to learn Angular",
+        postedOn: "13/09/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi.",
+        restrictedTo: [],
+    },
+    {
+        id: 4,
+        name: "Keshav",
+        title: "Stay Healthy",
+        postedOn: "05/12/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi. Lorem ipsum dolor sit amet cons m dolor sit amet consectetur adipisicing elit. Ipsa, se",
+        restrictedTo: [],
+    },
+    {
+        id: 5,
+        name: "Damodar",
+        title: "Keep Moving",
+        postedOn: "01/12/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi. Lorem ipsum dolor sit amet cons",
+        restrictedTo: [],
+    },
+    {
+        id: 6,
+        name: "Shankar",
+        title: "Be strong",
+        postedOn: "08/11/2022",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sequi. Lorem ipsum dolor sit amet cons",
+        restrictedTo: [],
+    }
+]
